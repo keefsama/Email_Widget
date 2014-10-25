517847196ce05333b73b4a74cd18d3615d1c598a
Keith Ferguson <keithferguson@optonline.net>
2014-10-25 12:26:44 -0400

    Latest Update with help from c0d3d. Script now running with input from widget

--- email_widget.py
+++ email_widget.py
@@ -2,39 +2,56 @@
 __author__ = 'kferguson'
 from Tkinter import *
 import poplib
-from email import parser
+import parser
+import sys
+
+username = ''
+password = ''
 
 # This portion is for Creation of the Widget we are going to use.
 def show_entry_fields():
-	print('Please Enter Username: %s\nPlease Enter Password: %s' % (e1.get(), e2.get()))
+    global username
+    global password
+    username = e1.get()
+    password = e2.get()
+    get_email(username, password)
 
+
+# This section is for connecting to gmail for import of all new emails.
+def get_email(user, passw):
+    user = str(user + "@gmail.com")
+    passw = str(passw)
+    pop_conn = poplib.POP3_SSL('pop.gmail.com')
+    print pop_conn.getwelcome()
+    pop_conn.user(user)
+    pop_conn.pass_(passw)
+    #Get messages from server:
+    print pop_conn.stat()
+    print pop_conn.list()
+    if pop_conn.stat()[1] > 0:
+        print("You have new mail.")
+        msgNum = len(pop_conn.list()[1])
+        print("You have %d messages in your inbox" % msgNum)
+        for msgList in range(msgNum):
+            for totMsg in pop_conn.retr(msgList + 1)[1]:
+                if totMsg.startswith('Subject'):
+                    print('\t' + totMsg)
+                    print("\n")
+                    break
+        pop_conn.quit()
+        sys.exit()
+    else:
+        print("No new mail.\n")
+        pop_conn.quit()
+
+#Remember to always define a function before calling it.
 master = Tk()
 Label(master, text='Please Enter Username:').grid(row=0)
 Label(master, text='Please Enter Password:').grid(row=1)
-
 e1 = Entry(master)
 e2 = Entry(master)
-
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
-
 Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
 Button(master, text='Submit', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
 mainloop()
-
-#This section is for connecting to gmail for import of all new emails.
-
-def get_email():
-	print(message)
-pop_conn = poplib.POP3_SSL('pop.gmail.com')
-pop_conn.user('username')
-pop_conn.pass_('password')
-#Get messages from server:
-messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
-# Concat message pieces:
-messages = ["\n".join(mssg[1]) for mssg in messages]
-#Parse message intom an email object:
-messages = [parser.Parser().parsestr(mssg) for mssg in messages]
-for message in messages:
-    print message['subject']
-pop_conn.quit()
